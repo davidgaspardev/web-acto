13dcb022966e30c89b2e459802b022647676c0a6
Eugod <eugenio28.04@gmail.com>
2024-06-26 19:17:25 -0300

    feat: creating OurUnitys component

---
+++ app/(home)/home/components/OurUnitys.tsx
@@ -0,0 +1,26 @@
+import styles from "./OurUnitys.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+export const OurUnitys = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.subContainer}>
+                <Image 
+                    unoptimized 
+                    src={"https://www.oystr.com.br/wp-content/uploads/2022/08/placeholder.png"} 
+                    alt="Placeholder" 
+                    width={300} 
+                    height={300} 
+                    className={styles.image}
+                />
+
+                <div className={styles.textContainer}>
+                    <p className={styles.title}> Conheça nossas unidade</p>
+                    <p className={styles.subtitle}>Encontre a unidade ideal para você mudar a sua vida</p>
+                    <Link href={"https://www.youtube.com"} className={styles.link}>Saiba mais</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -1,7 +1,11 @@
+import { OurUnitys } from "./components/OurUnitys";
+
 export default function HomePage() {
   return (
     <div className="h-screen">
       <h1>Home Page</h1>
+
+      <OurUnitys />
     </div>
   );
 }
