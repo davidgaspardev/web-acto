2ce7f420e13459807fdd8e0f69a0b360ff183e30
David Gaspar <davidgaspar.dev@gmail.com>
2024-07-01 13:06:33 -0300

    refactor: Update max width value for home page container

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -6,7 +6,7 @@ import { cardModelOneData } from "@/helpers/data";
 
 export default function HomePage() {
   return (
-    <div className="max-w-7xl m-auto">
+    <div className="max-w-6xl m-auto">
       <Carousel className="p-4" thumbnails={[
         'https://firebasestorage.googleapis.com/v0/b/acto-dev.appspot.com/o/images%2Fimg-banner-01.png?alt=media&token=7f7dd4f8-1825-4e43-83c0-233dc0ccbbf3',
         'https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-800x525.jpg',
