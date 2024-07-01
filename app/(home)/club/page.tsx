d98bcd1d5ed7f3079b0391d4f3571d83317fe6be
David Gaspar <davidgaspar.dev@gmail.com>
2024-07-01 20:12:26 -0300

    chore: Add page base to Club page

---
+++ app/(home)/club/page.tsx
@@ -0,0 +1,7 @@
+export default function ClubPage() {
+  return (
+    <div className="max-w-6xl m-auto">
+      <h1>Hello World for Club Page</h1>
+    </div>
+  );
+}
--- app/(home)/components/Header.tsx
+++ app/(home)/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
             },
             {
               name:  "Clube",
-              link: "https://firebasestorage.googleapis.com/v0/b/acto-dev.appspot.com/o/club%2Facto_club.pdf?alt=media&token=0fef94d5-694f-4b51-8893-1f804435839c"
+              link: "/club"
             }
           ].map(({ name, link }, index) => (
             <Link key={index} href={link} className="h-full w-24 flex flex-col items-center justify-center">
