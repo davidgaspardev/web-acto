53a7f8733c40649963b2400b8168089df6cf307a
David Gaspar <davidgaspar.dev@gmail.com>
2024-07-08 13:30:14 -0300

    chore: Disable Club menu

--- app/(home)/components/Header.tsx
+++ app/(home)/components/Header.tsx
@@ -14,10 +14,10 @@ export default function Header() {
               name: "Planos",
               link: "/plans"
             },
-            {
-              name:  "Clube",
-              link: "https://firebasestorage.googleapis.com/v0/b/acto-dev.appspot.com/o/club%2Facto_club.pdf?alt=media&token=0fef94d5-694f-4b51-8893-1f804435839c"
-            }
+            // {
+            //   name:  "Clube",
+            //   link: "https://firebasestorage.googleapis.com/v0/b/acto-dev.appspot.com/o/club%2Facto_club.pdf?alt=media&token=0fef94d5-694f-4b51-8893-1f804435839c"
+            // }
           ].map(({ name, link }, index) => (
             <Link key={index} href={link} className="h-full w-24 flex flex-col items-center justify-center">
               <h2 className="font-Bree font-bold text-xl text-[#553581]">{name}</h2>
